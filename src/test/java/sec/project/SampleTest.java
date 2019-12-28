579241349cfbdf8a982268879cc9b04023d4c3be
Jhoneagle <eaglekjohn@gmail.com>
2019-12-28 11:26:02 +0200

    update dependencies

---
+++ src/test/java/sec/project/SampleTest.java
@@ -0,0 +1,15 @@
+package sec.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class SampleTest {
+    @Test
+    public void mock() {
+        assertTrue(true);
+    }
+}
